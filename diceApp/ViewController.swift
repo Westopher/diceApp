1fab52d0b7b9e44da84c907d53f57dda5873962a
West Kraemer <kraemerwest@gmail.com>
2019-02-19 16:45:22 -0500

    add ui connections

--- diceApp/ViewController.swift
+++ diceApp/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    @IBAction func rollButtonPressed(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
