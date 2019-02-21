58d36a252cfa3e8873157a4ba3cc54e04741da15
West Kraemer <kraemerwest@gmail.com>
2019-02-21 14:13:37 -0500

    add array'

--- diceApp/ViewController.swift
+++ diceApp/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
     var randomDiceIndex1: Int = 0
     var randomDiceIndex2: Int = 0
     
@@ -22,8 +22,8 @@ class ViewController: UIViewController {
         randomDiceIndex2 = Int.random(in: 0 ... 5)
         print(randomDiceIndex1)
         
-        diceImageView1.image = UIImage(named: "dice\(randomDiceIndex1)")
-        diceImageView2.image = UIImage(named: "dice\(randomDiceIndex1)")
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
     
     override func viewDidLoad() {
